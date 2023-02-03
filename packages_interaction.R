b83f9014d00aa671f03b7973e94c27d3408ef34a
collazoa <anja.collazo@charite.de>
2023-02-03 14:23:07 +0100

    packages to run R-script for Fig1 and Fig1S

--- packages_interaction.R
+++ packages_interaction.R
@@ -1,8 +1,9 @@
-# packages for sample size / interaction analysis 
 
-library(Superpower)
-library(tidyverse)
-library(gridExtra)
-library(grid)
-library(plotly)
+# set up for running the R-script 
 
+pkgs <- c("Superpower", "tidyverse", "gridExtra", "grid", "plotly", "WebPower", "easypackages")
+ipkgs <- rownames(installed.packages()) 
+npkgs <- setdiff(pkgs, ipkgs)
+if (length(npkgs) > 0) install.packages(npkgs) 
+easypackages::libraries(pkgs)
+for(p in pkgs) print(sprintf("Package %s, version=%s", p, packageVersion(pkg = p)))
